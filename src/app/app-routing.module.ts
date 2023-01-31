e9a520d764e64f30594dac1cb55676a66ef39883
Alfred Benavent <abenavent@okode.com>
2023-01-31 09:14:21 +0100

    Add router link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'mfe-navigation',
+    loadChildren: () => loadRemoteModule( 'mfe', './Request').then((m) => { console.log(m); return m.RequestModule }),
+    data: { origin: 'verti' }
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
